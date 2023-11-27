66eb2216b455119753928141320304354e0f76ae
projectcdcc <projectcdc@gmail.com>
2023-11-27 13:37:52 +0700

    Profile picture added to employer profile details

--- resources/views/employer/emp-profile-detail.blade.php
+++ resources/views/employer/emp-profile-detail.blade.php
@@ -16,8 +16,25 @@
                     <h3 class="text-base font-semibold leading-7 text-gray-900">Profile Details</h3>
                     <p class="mt-1 max-w-2xl text-sm leading-6 text-gray-500">Organization details</p>
                 </div>
+
+
+
                 <div class="mt-6 border-t border-gray-100">
                     <dl class="divide-y divide-gray-100">
+
+                        <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                            <dt class="text-sm font-medium leading-6 text-gray-900">Profile Picture</dt>
+                            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                                <!-- Profile Picture Section -->
+                                    @if(Auth::user()->avatar)
+                                        <img src="/avatars/{{Auth::user()->avatar}}" alt="Profile Picture" class="h-20 w-20 rounded-full object-cover">
+                                    @else
+                                        <!-- Placeholder image or default profile picture -->
+                                        <img src="{{ asset('path-to-placeholder-image.jpg') }}" alt="Default Profile Picture" class="h-20 w-20 rounded-full object-cover">
+                                    @endif
+                                </dd>
+                        </div>
+
                         <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
                             <dt class="text-sm font-medium leading-6 text-gray-900">Organization name</dt>
                             <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{ $employer->organization_name}}</dd>
