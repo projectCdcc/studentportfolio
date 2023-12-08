4470278801b22179a11f507cfcbddf2f76bd556d
projectCdcc <147791087+projectCdcc@users.noreply.github.com>
2023-12-08 19:05:30 +0700

    Added go back button on Login and Register "UI"

--- resources/views/student/student-register.blade.php
+++ resources/views/student/student-register.blade.php
@@ -41,6 +41,17 @@
         </div>
 
         <div class="flex items-center justify-end mt-4">
+
+            <!-- Go back button -->
+            <a  href="{{ url()->previous() }}">
+                <button type="button" class="mr-10 w-full flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700">
+                    <svg class="w-5 h-5 rtl:rotate-180" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
+                    </svg>
+                    <span>Go back</span>
+                </button>
+            </a>
+
             <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
                 {{ __('Already registered?') }}
             </a>
